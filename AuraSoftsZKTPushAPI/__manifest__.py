b4a4d6042fa74e367d661ebdb679290ca130a642
xeechaun <zeeshan.310@gmail.com>
2025-03-21 20:45:58 +0300

    Update __manifest__.py

--- AuraSoftsZKTPushAPI/__manifest__.py
+++ AuraSoftsZKTPushAPI/__manifest__.py
@@ -1,1 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "AuraSofts ZKTeco Push SDK/API",
+    "version": "1.0",
+    "category": "Generic Modules/Human Resources/Access Control",
+    "sequence": 1,
+    "summary": "Receive and manage attendance and user data with biometric devices",
+    "depends": [],
+    "description": """
 
+
+It supports:
+
+- ZKTeco
+
+- eSSL
+
+This module requires a valid API license.
+Contact us for pricing structure.
+https://aurasofts.com/zkteco-fingerprint-attendance/
+    """,
+    "author": "AuraSofts",
+    "category": "Generic Modules/Human Resources",
+    "version": "1.0",
+    "license": "Proprietary",
+    "depends": ["hr","hr_attendance"],
+    "installable": True,
+}
